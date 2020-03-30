62c37e946a6ab33fc87722ba394dbd8efaa9a2bf
Francisco Fernández <ferrycosv@gmail.com>
2020-03-30 11:15:10 +0200

    All done

--- setters/refactor-1.js
+++ setters/refactor-1.js
@@ -7,19 +7,19 @@ const obj1 = {
 
 const obj2 = {
   greeting: ``,
-  set greetingName(newName) {
-    // write me!
+  setGreetingName: function (newName) {
+    this.greeting = `hi, I'm ${newName}!`;
   }
 };
 
 obj1.setGreetingName('obj1');
 console.assert(obj1.greeting === "hi, I'm obj1!", 'Test 1');
 
-; // write this line
+obj2.setGreetingName('obj2');
 console.assert(obj2.greeting === "hi, I'm obj2!", 'Test 2');
 
 obj1.setGreetingName('hi');
 console.assert(obj1.greeting === "hi, I'm hi!", 'Test 3');
 
-; // write this line
+obj2.setGreetingName('bye');
 console.assert(obj2.greeting === "hi, I'm bye!", 'Test 4');
